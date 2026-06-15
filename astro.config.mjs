bdccc993c0d58f1b6babd7d2ecd1813f6568c22f
LuisCruzA <langel18.1818@gmail.com>
2026-06-15 10:47:20 -0600

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon'; // <-- Importación corregida
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [
+    icon() // <-- Sin el punto y coma
+  ],  
+});
