786dd1adc98105c9f1ab9e13530a345d477fd412
LuisCruzA <langel18.1818@gmail.com>
2026-06-20 11:03:25 -0600

    cambio de ruta de imagenes

--- src/data.ts
+++ src/data.ts
@@ -4,21 +4,21 @@ export const servicesData : ServiceProps[] = [
   {
     title: "Agua Purificada",
     description: "Garrafones de 20 litros directo hasta la puerta de tu hogar. Nuestro precio al público general es de $25, pero si buscas surtir tu negocio, contamos con precios especiales para revendedores. ¡Pide tu servicio a domicilio hoy mismo!",
-    imageSrc: "/src/assets/garrafon.png", 
+    imageSrc: "/garrafon.png", 
     imageAlt: "garrafon de agua purificada",
     iconType: "agua"
   },
   {
     title: "Pipas de Agua Potable",
     description: "Servicio de abastecimiento rápido y seguro para cisternas, albercas o construcciones. Contamos con unidades de diferentes capacidades, incluyendo 5,000 y 10,000 litros. Contáctanos para recibir más información y agendar tu viaje.",
-    imageSrc: "/src/assets/pipa.jpeg",
+    imageSrc: "/pipa.jpeg",
     imageAlt: "Pipa de Agua de 10 mil litros",
     iconType: "domicilio"
   },
   {
     title: "Maquinaria y Acarreo",
     description: "Apoyamos tus proyectos de construcción con servicio de renta de camiones de volteo y retroexcavadoras. También realizamos acarreo de materiales de forma eficiente. Comunícate con nosotros para consultar detalles de precios y servicios.",
-    imageSrc: "/src/assets/volteo.jpg", 
+    imageSrc: "/volteo.jpg", 
     imageAlt: "Volteo de carga de material",
     iconType: "calidad"
   },
